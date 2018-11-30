7d54a1f27689effc4dc2032298b419786e6ae3e8
Victor Tang <victorwtang@yahoo.com>
2018-11-29 17:17:19 -0800

    Added files to header

--- parser.h
+++ parser.h
@@ -1,10 +1,21 @@
 #ifndef PARSER_H
 #define PARSER_H
-
+#include <cstdlib>
+#include <string>
 
 class parser
 {
 public:
+  enum ERRORS {INVALID_INPUT};
+
+  // Takes in algebraic expression, returns RPN expression
+  static std::string parseExpression(const std::string &input);
+
+private:
+  // Determines if algebraic expression is valid
+  static bool isInputValid(const std::string &input);
+  // Translates algebraic expression to RPN expression
+  static std::string translateExpression(const std::string &input);
   parser();
 };
 
