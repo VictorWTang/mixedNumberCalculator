100333cec0c5e632f361bce09f03d6007d3342ad
Victor Tang <victorwtang@yahoo.com>
2018-12-05 16:32:17 -0800

    Formatting changes

--- parser.cpp
+++ parser.cpp
@@ -2,206 +2,206 @@
 
 std::vector<std::string> parser::strToVector(std::string input)
 {
-    std::vector<std::string> expVect;
+  std::vector<std::string> expVect;
 
-    std::queue<char> line;
-    std::string send;
+  std::queue<char> line;
+  std::string send;
 
-    //put input into a queue
-    for(int i = 0; i < input.size(); i ++)
-        line.push(input[i]);
+  //put input into a queue
+  for(int i = 0; i < input.size(); i ++)
+    line.push(input[i]);
 
-    char firstCheck = line.front();
+  char firstCheck = line.front();
 
-    //in case first number is negative
-    if(firstCheck == '-')
+  //in case first number is negative
+  if(firstCheck == '-')
+  {
+    send += firstCheck;
+    line.pop();
+  }
+  firstCheck = ' ';
+
+  char check;
+  //go through queue and add to expVect, string to vector
+  do
+  {
+    check = line.front();
+    if(isdigit(check) || check == '.' || check == ' ')
     {
-        send += firstCheck;
+      if(check == ' ')
+      {
+        send += check;
         line.pop();
-    }
-    firstCheck = ' ';
-
-    char check;
-    //go through queue and add to expVect, string to vector
-    do
-    {
         check = line.front();
-        if(isdigit(check) || check == '.' || check == ' ')
+        do
         {
-            if(check == ' ')
-            {
-                send += check;
-                line.pop();
-                check = line.front();
-                do
-                {
-                    if(check != ' ')
-                        send += check;
-                    line.pop();
-                    check = line.front();
-                }while(check != '/');
-            }
+          if(check != ' ')
             send += check;
-            line.pop();
+          line.pop();
+          check = line.front();
+        }while(check != '/');
+      }
+      send += check;
+      line.pop();
+    }
+    else
+    {
+      if(!send.empty())
+      {
+        expVect.push_back(send);
+        send.clear();
+      }
+      send += check;
+      expVect.push_back(send);
+      send.clear();
+      line.pop();
+      char c;
+      c = line.front();
+      if(c == '-' || c == ' ')
+        do
+      {
+        if(c == '-')
+        {
+          send += c;
+          line.pop();
         }
         else
         {
-            if(!send.empty())
-            {
-                expVect.push_back(send);
-                send.clear();
-            }
-            send += check;
-            expVect.push_back(send);
-            send.clear();
-            line.pop();
-            char c;
-            c = line.front();
-            if(c == '-' || c == ' ')
-                do
-                {
-                    if(c == '-')
-                    {
-                        send += c;
-                        line.pop();
-                    }
-                    else
-                    {
-                        line.pop();
-                        c = line.front();
-                    }
-                }while(c == ' ');
+          line.pop();
+          c = line.front();
         }
-    }while(!line.empty());
+      }while(c == ' ');
+    }
+  }while(!line.empty());
 
-    if(!send.empty())
-        expVect.push_back(send);
+  if(!send.empty())
+    expVect.push_back(send);
 
-    return expVect;
+  return expVect;
 }
 
 std::string parser::toRPN(std::vector<std::string> exp)
 {
-    std::vector<std::string> RPNVect;
+  std::vector<std::string> RPNVect;
 
-    std::stack<std::string> LIFO;
-    std::queue<std::string> FIFO;
+  std::stack<std::string> LIFO;
+  std::queue<std::string> FIFO;
 
-    char last;
+  char last;
 
-    for(int i = 0; i < exp.size(); i++)
+  for(int i = 0; i < exp.size(); i++)
+  {
+    std::string check = exp[i];
+    char next = check[0];
+
+    if(isdigit(next) || (next == '-' && check.size() > 1))
     {
-        std::string check = exp[i];
-        char next = check[0];
-
-        if(isdigit(next) || (next == '-' && check.size() > 1))
-        {
-            FIFO.push(check);
-        }
-        else if(LIFO.empty() && !isdigit(next))
-        {
-            LIFO.push(check);
-        }
-        else if(check =="+" || check =="-" || check =="*" || check =="/")
-        {
-            int inStack, nextValue;
-            inStack = getPrecedence(LIFO.top());
-            nextValue = getPrecedence(check);
-            if(inStack < nextValue)
-                LIFO.push(check);
-            else
-            {
-                do
-                {
-                    FIFO.push(LIFO.top());
-                    LIFO.pop();
-                    if(!LIFO.empty())
-                        inStack = getPrecedence(LIFO.top());
-                }while(inStack >= nextValue && !LIFO.empty());
-                LIFO.push(check);
-            }
-        }
-        else if(check == "(" || check == ")")
-        {
-            switch(next)
-            {
-            case '(':
-                if(isdigit(last))
-                    LIFO.push("*");
-                LIFO.push(check);
-                break;
-            case ')':
-                do
-                {
-                    FIFO.push(LIFO.top());
-                    LIFO.pop();
-
-                }while(LIFO.top() != "(");
-                if(LIFO.top() == "(")
-                {
-                    LIFO.pop();
-                }
-                break;
-            }
-        }
-        else
-        {
-            throw TRANSLATE_ERROR;
-            std::cout<<check<<std::endl;
-        }
-        last = next;
+      FIFO.push(check);
     }
-    if(!LIFO.empty())
+    else if(LIFO.empty() && !isdigit(next))
     {
+      LIFO.push(check);
+    }
+    else if(check =="+" || check =="-" || check =="*" || check =="/")
+    {
+      int inStack, nextValue;
+      inStack = getPrecedence(LIFO.top());
+      nextValue = getPrecedence(check);
+      if(inStack < nextValue)
+        LIFO.push(check);
+      else
+      {
         do
         {
-            FIFO.push(LIFO.top());
-            LIFO.pop();
-        }while(!LIFO.empty());
+          FIFO.push(LIFO.top());
+          LIFO.pop();
+          if(!LIFO.empty())
+            inStack = getPrecedence(LIFO.top());
+        }while(inStack >= nextValue && !LIFO.empty());
+        LIFO.push(check);
+      }
     }
-    do{
-        RPNVect.push_back(FIFO.front());
-        FIFO.pop();
-
-    }while(!FIFO.empty());
-
-    //for the calculate call, should there be spaces between?
-    std::string send;
-    for(int i = 0; i < RPNVect.size(); i++)
+    else if(check == "(" || check == ")")
     {
-        send += RPNVect[i];
-        send += " ";
+      switch(next)
+      {
+      case '(':
+        if(isdigit(last))
+          LIFO.push("*");
+        LIFO.push(check);
+        break;
+      case ')':
+        do
+        {
+          FIFO.push(LIFO.top());
+          LIFO.pop();
+
+        }while(LIFO.top() != "(");
+        if(LIFO.top() == "(")
+        {
+          LIFO.pop();
+        }
+        break;
+      }
     }
-    return send;
+    else
+    {
+      throw TRANSLATE_ERROR;
+      std::cout<<check<<std::endl;
+    }
+    last = next;
+  }
+  if(!LIFO.empty())
+  {
+    do
+    {
+      FIFO.push(LIFO.top());
+      LIFO.pop();
+    }while(!LIFO.empty());
+  }
+  do{
+    RPNVect.push_back(FIFO.front());
+    FIFO.pop();
+
+  }while(!FIFO.empty());
+
+  //for the calculate call, should there be spaces between?
+  std::string send;
+  for(int i = 0; i < RPNVect.size(); i++)
+  {
+    send += RPNVect[i];
+    send += " ";
+  }
+  return send;
 }
 
 int parser::getPrecedence(std::string str)
 {
-    char s = str[0];
-    switch(s)
-    {
-    case '+':
-        return 0;
-        break;
-    case '-':
-        return 0;
-        break;
-    case '*':
-        return 1;
-        break;
-    case '/':
-        return 1;
-        break;
-    default:
-        return -1;
-    }
+  char s = str[0];
+  switch(s)
+  {
+  case '+':
+    return 0;
+    break;
+  case '-':
+    return 0;
+    break;
+  case '*':
+    return 1;
+    break;
+  case '/':
+    return 1;
+    break;
+  default:
+    return -1;
+  }
 }
 
 std::string parser::parseToRPN(const std::string  &input)
 {
-    std::string send;
-    send = toRPN(strToVector(input));
-    return send;
+  std::string send;
+  send = toRPN(strToVector(input));
+  return send;
 }
 
 void parser::ensureInputValid(const std::string &input) {
@@ -291,25 +291,25 @@ void parser::ensureInputValid(const std::string &input) {
   }
 
 
-//  char nextChar;
-//  while(ss.good()) {
-//    nextChar = static_cast<char>(ss.peek());
-//    if(isNumber(nextChar)) {
+  //  char nextChar;
+  //  while(ss.good()) {
+  //    nextChar = static_cast<char>(ss.peek());
+  //    if(isNumber(nextChar)) {
 
-//    } else if(isOperator(nextChar)) {
+  //    } else if(isOperator(nextChar)) {
 
-//    } else if(nextChar == '(') {
-//      leftParenCount++; // TODO Can a left bracket come after a number?
-//    } else if(nextChar == ')') {
-//      rightParenCount++;
-//      if(operatorFound) {
-//        throw INVALID_INPUT;
-//      }
-//      if(rightParenCount > leftParenCount) {
-//        throw INVALID_INPUT;
-//      }
-//    } else if(nextChar )
-//  }
+  //    } else if(nextChar == '(') {
+  //      leftParenCount++; // TODO Can a left bracket come after a number?
+  //    } else if(nextChar == ')') {
+  //      rightParenCount++;
+  //      if(operatorFound) {
+  //        throw INVALID_INPUT;
+  //      }
+  //      if(rightParenCount > leftParenCount) {
+  //        throw INVALID_INPUT;
+  //      }
+  //    } else if(nextChar )
+  //  }
 }
 
 
