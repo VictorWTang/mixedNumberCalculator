c11f6063aaa4023985c14270fbeba7501181a05f
Victor Tang <victorwtang@yahoo.com>
2018-12-05 16:28:38 -0800

    Added new functions to parse class

--- parser.cpp
+++ parser.cpp
@@ -1,11 +1,207 @@
 #include "parser.h"
 
-std::string parser::parseToRPN(const std::string  &input) {
+std::vector<std::string> parser::strToVector(std::string input)
+{
+    std::vector<std::string> expVect;
 
+    std::queue<char> line;
+    std::string send;
+
+    //put input into a queue
+    for(int i = 0; i < input.size(); i ++)
+        line.push(input[i]);
+
+    char firstCheck = line.front();
+
+    //in case first number is negative
+    if(firstCheck == '-')
+    {
+        send += firstCheck;
+        line.pop();
+    }
+    firstCheck = ' ';
+
+    char check;
+    //go through queue and add to expVect, string to vector
+    do
+    {
+        check = line.front();
+        if(isdigit(check) || check == '.' || check == ' ')
+        {
+            if(check == ' ')
+            {
+                send += check;
+                line.pop();
+                check = line.front();
+                do
+                {
+                    if(check != ' ')
+                        send += check;
+                    line.pop();
+                    check = line.front();
+                }while(check != '/');
+            }
+            send += check;
+            line.pop();
+        }
+        else
+        {
+            if(!send.empty())
+            {
+                expVect.push_back(send);
+                send.clear();
+            }
+            send += check;
+            expVect.push_back(send);
+            send.clear();
+            line.pop();
+            char c;
+            c = line.front();
+            if(c == '-' || c == ' ')
+                do
+                {
+                    if(c == '-')
+                    {
+                        send += c;
+                        line.pop();
+                    }
+                    else
+                    {
+                        line.pop();
+                        c = line.front();
+                    }
+                }while(c == ' ');
+        }
+    }while(!line.empty());
+
+    if(!send.empty())
+        expVect.push_back(send);
+
+    return expVect;
 }
 
-std::string parser::formatExpression(const std::string &input) {
+std::string parser::toRPN(std::vector<std::string> exp)
+{
+    std::vector<std::string> RPNVect;
 
+    std::stack<std::string> LIFO;
+    std::queue<std::string> FIFO;
+
+    char last;
+
+    for(int i = 0; i < exp.size(); i++)
+    {
+        std::string check = exp[i];
+        char next = check[0];
+
+        if(isdigit(next) || (next == '-' && check.size() > 1))
+        {
+            FIFO.push(check);
+        }
+        else if(LIFO.empty() && !isdigit(next))
+        {
+            LIFO.push(check);
+        }
+        else if(check =="+" || check =="-" || check =="*" || check =="/")
+        {
+            int inStack, nextValue;
+            inStack = getPrecedence(LIFO.top());
+            nextValue = getPrecedence(check);
+            if(inStack < nextValue)
+                LIFO.push(check);
+            else
+            {
+                do
+                {
+                    FIFO.push(LIFO.top());
+                    LIFO.pop();
+                    if(!LIFO.empty())
+                        inStack = getPrecedence(LIFO.top());
+                }while(inStack >= nextValue && !LIFO.empty());
+                LIFO.push(check);
+            }
+        }
+        else if(check == "(" || check == ")")
+        {
+            switch(next)
+            {
+            case '(':
+                if(isdigit(last))
+                    LIFO.push("*");
+                LIFO.push(check);
+                break;
+            case ')':
+                do
+                {
+                    FIFO.push(LIFO.top());
+                    LIFO.pop();
+
+                }while(LIFO.top() != "(");
+                if(LIFO.top() == "(")
+                {
+                    LIFO.pop();
+                }
+                break;
+            }
+        }
+        else
+        {
+            throw TRANSLATE_ERROR;
+            std::cout<<check<<std::endl;
+        }
+        last = next;
+    }
+    if(!LIFO.empty())
+    {
+        do
+        {
+            FIFO.push(LIFO.top());
+            LIFO.pop();
+        }while(!LIFO.empty());
+    }
+    do{
+        RPNVect.push_back(FIFO.front());
+        FIFO.pop();
+
+    }while(!FIFO.empty());
+
+    //for the calculate call, should there be spaces between?
+    std::string send;
+    for(int i = 0; i < RPNVect.size(); i++)
+    {
+        send += RPNVect[i];
+        send += " ";
+    }
+    return send;
+}
+
+int parser::getPrecedence(std::string str)
+{
+    char s = str[0];
+    switch(s)
+    {
+    case '+':
+        return 0;
+        break;
+    case '-':
+        return 0;
+        break;
+    case '*':
+        return 1;
+        break;
+    case '/':
+        return 1;
+        break;
+    default:
+        return -1;
+    }
+}
+
+std::string parser::parseToRPN(const std::string  &input)
+{
+    std::string send;
+    send = toRPN(strToVector(input));
+    return send;
 }
 
 void parser::ensureInputValid(const std::string &input) {
--- parser.h
+++ parser.h
@@ -3,6 +3,9 @@
 #include <cstdlib>
 #include <string>
 #include <sstream>
+#include <stack>
+#include <queue>
+#include <vector>
 #include "streamutilities.h"
 
 class parser
@@ -17,7 +20,8 @@ public:
     OPERATOR_AFTER_OPERATOR,
     DECIMAL_WITHOUT_PROCEEDING_NUMBER,
     DECIMAL_WITHOUT_PRECEEDING_NUMBER,
-    INVALID_CHARACTER
+    INVALID_CHARACTER,
+    TRANSLATE_ERROR
   };
 
   // Takes in algebraic expression, returns RPN expression
@@ -28,6 +32,10 @@ public:
   static std::string formatExpression(const std::string &input);
 
 private:
+  static std::string toRPN(std::vector<std::string>);
+  static std::vector<std::string> strToVector(std::string);
+  static int getPrecedence(std::string);
+
   // Determines if algebraic expression is valid
   // Throws INVALID_INPUT if input not valid
   static void ensureInputValid(const std::string &input);
